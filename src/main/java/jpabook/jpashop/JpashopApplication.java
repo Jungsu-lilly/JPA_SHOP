de95ca21196177e4dc2f2c197703757a8532e3c5
matt <matt1235@naver.com>
2022-06-29 21:10:15 +0900

    Application

---
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -0,0 +1,19 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+@SpringBootApplication
+public class JpashopApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+}
