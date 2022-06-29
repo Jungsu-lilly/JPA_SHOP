c55a691c514cc5cadc8098599de3f731139a56ed
matt <matt1235@naver.com>
2022-06-29 21:46:42 +0900

    OrderRepository, OrderSearch

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.criteria.*;
 import java.util.List;
 
 @Repository
@@ -21,5 +22,13 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-    // public List<Order> findAll(ORderSearch orderSearch)
+    public List<Order> findAll(OrderSearch orderSearch){
+        return em.createQuery("select o from Order o join o.member m "+
+                "where o.status = :status "+ "and m.name like :name", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                .setMaxResults(1000)   // 최대 1000건
+                .getResultList();
+    }
+
 }
